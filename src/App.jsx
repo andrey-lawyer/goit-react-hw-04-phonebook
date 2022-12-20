70a1d73abd16ce44228c0afd10a4cca6b1ff40b8
Andrey Uvarov <andrey-1@ukr.net>
2022-12-20 09:57:19 +0200

    comit7

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ class App extends Component {
     } catch (error) {
       parseState = emptyArray;
     }
+
     this.setState({ contacts: parseState });
   }
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -11,6 +11,7 @@ class ContactForm extends Component {
     number: '',
   };
   handleSubmit = e => {
+    e.preventDefault();
     this.props.onSubmit(this.state);
     this.reset();
   };
